768b13c670815d2e0d635a3b09ebdcc970af33ba
Stanisław Jakubowski <sjakubowski3554@gmail.com>
2025-05-17 14:59:59 +0200

    feat: add cli handling

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: bittorrent <torrent-file> <output-directory>")
+		os.Exit(1)
+	}
+
+	torrentFilePath := os.Args[1]
+
+	content, err := os.ReadFile(torrentFilePath)
+	if err != nil {
+		fmt.Println("Error reading torrent file:", err)
+		os.Exit(1)
+	}
+
+	outputDir := os.Args[2]
+
+	_, err = os.Stat(outputDir)
+	if err != nil {
+		fmt.Println("Error checking output directory:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(string(content))
+
+	fmt.Println(
+		"BitTorrent client initialized. Download would start here in a complete implementation.",
+	)
 }
